a2dcc46ced30c99c61cb72bdea284256c916b1fd
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-06-13 23:20:59 +0530

    Daily wage of Emp UC2

--- Emp-Wage/Program.cs
+++ Emp-Wage/Program.cs
@@ -4,16 +4,21 @@ namespace Emp_Wage
 {
     internal class EmpWageAssigment
     {
+        static int fullday = 8;
+        static int wageperhour = 20;
         static void Main(string[] args)
         {
-            //Checking emp is present of absent
 
+            //Calculating daily wage of the Employee
+            
             Random check = new Random();
             int presentorabsent = check.Next(0, 2);
             Console.WriteLine(presentorabsent);
             if (presentorabsent == 1)
             {
                 Console.WriteLine("Emp is present");
+                int dailywage = fullday * wageperhour;
+                Console.WriteLine("Daily wage of the Emp is " + dailywage);
             }
             else
             {
